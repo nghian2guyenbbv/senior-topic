4b566611c3e9aa64f3d68dc5054bf78bbaa7320c
nghianguyenbbv <nghia2nguyen@bbv.vn>
2026-03-19 17:30:33 +0700

    ATLAS-0000: init jwt with gateway + gitignote + init project

---
+++ src/test/java/org/spring/oauth/server/jwtspringgateway/JwtSpringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spring.oauth.server.jwtspringgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
