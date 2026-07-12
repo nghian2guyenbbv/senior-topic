904b602be58b3cd19a83ef5639bfea79b0740a9f
nghia2nguyen <nghia2.nguyen@sag-ag.ch>
2026-07-12 17:02:13 +0700

    ATLAS-0000: init rag - spring-ai-rag dependency

--- src/main/java/org/spring/streaming/ragollama/config/ChatClientConfig.java
+++ src/main/java/org/spring/streaming/ragollama/config/ChatClientConfig.java
@@ -6,6 +6,8 @@ import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,12 +17,21 @@ import java.util.List;
 @Configuration
 public class ChatClientConfig {
   @Bean
-  public ChatClient configChatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory, VectorStore vectorStore) {
+  public ChatClient configChatClient(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory,
+      QuestionAnswerAdvisor questionAnswerAdvisor, RetrievalAugmentationAdvisor retrievalAugmentationAdvisor) {
     Advisor logAdvisor = new SimpleLoggerAdvisor();
-    Advisor memoryAdvisor = MessageChatMemoryAdvisor
-        .builder(chatMemory).build();
-    Advisor ragAdvisor = QuestionAnswerAdvisor.builder(vectorStore).build();
-    return chatClientBuilder.defaultAdvisors(List.of(logAdvisor, memoryAdvisor, ragAdvisor)).build();
+    Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+    return chatClientBuilder.defaultAdvisors(List.of(logAdvisor, memoryAdvisor, retrievalAugmentationAdvisor)).build();
   }
 
+  @Bean
+  public QuestionAnswerAdvisor questionAnswerAdvisor(VectorStore vectorStore) {
+    return QuestionAnswerAdvisor.builder(vectorStore).build();
+  }
+
+  @Bean
+  public RetrievalAugmentationAdvisor retrievalAugmentationAdvisor(VectorStore vectorStore) {
+    return RetrievalAugmentationAdvisor.builder()
+        .documentRetriever(VectorStoreDocumentRetriever.builder().vectorStore(vectorStore).build()).build();
+  }
 }
